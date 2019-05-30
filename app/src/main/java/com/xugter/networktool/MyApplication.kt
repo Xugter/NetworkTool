5885621013a02b2d448af20a83a139aa054452b6
Xugter <xubomain@163.com>
2019-05-30 19:53:09 +0800

    初始化

---
+++ app/src/main/java/com/xugter/networktool/MyApplication.kt
@@ -0,0 +1,11 @@
+package com.xugter.networktool
+
+import android.app.Application
+import com.xugter.networktoollib.NetworkTool
+
+class MyApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        NetworkTool.init(this)
+    }
+}
